ac0b975e6bbbd3f46c9c7ba0a28fcea1fe33bbd7
Giuliano Giusti <giuliano.giusti61@gmail.com>
2021-07-18 20:15:07 +0200

    translation update

---
+++ lang/it/lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Giuliano Giusti <giuliano.giusti61@gmail.com>
+ */
+$lang['yearbox_months_jan']    = 'Gen';
+$lang['yearbox_months_feb']    = 'Feb';
+$lang['yearbox_months_mar']    = 'Mar';
+$lang['yearbox_months_apr']    = 'Apr';
+$lang['yearbox_months_may']    = 'Mag';
+$lang['yearbox_months_jun']    = 'Giu';
+$lang['yearbox_months_jul']    = 'Lug';
+$lang['yearbox_months_aug']    = 'Ago';
+$lang['yearbox_months_sep']    = 'Set';
+$lang['yearbox_months_oct']    = 'Ott';
+$lang['yearbox_months_nov']    = 'Nov';
+$lang['yearbox_months_dec']    = 'Dic';
+$lang['yearbox_days']          = 'DLMMGVS';

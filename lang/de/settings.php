bf74a74ef47e4bba7220225fd66e68f45e0d052f
Ralf Schmitt <ralf_schmitt@yahoo.de>
2022-01-27 07:40:18 +0100

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Ralf Schmitt <ralf_schmitt@yahoo.de>
+ */
+$lang['namestructure']         = 'The naming scheme used for creating new entries and finding the entries to display.
+';
+$lang['namestructure_o_default'] = '<Wurzel> : Jahr-Monat : <Name>-Jahr-Monat-Tag';
+$lang['namestructure_o_YearMonthSeperatedNS'] = '<Wurzel> : Jahr : Monat : <Name>-Jahr-Monat-Tag';
+$lang['namestructure_o_separatedCompletely'] = '<Wurzel> : Jahr : Monat : Tag';
+$lang['namestructure_o_YearNS'] = '<Wurzel> : Jahr : <Name>-Jahr-Monat-Tag';

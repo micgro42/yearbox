0064b3f61331a860d771df11f15f9f97435c330e
Hiroki Watanabe <hwatanabe.japan@gmail.com>
2024-05-21 16:40:04 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hiroki Watanabe <hwatanabe.japan@gmail.com>
+ */
+$lang['yearbox_months_jan']    = '1月';
+$lang['yearbox_months_feb']    = '2月';
+$lang['yearbox_months_mar']    = '3月';
+$lang['yearbox_months_apr']    = '4月';
+$lang['yearbox_months_may']    = '5月';
+$lang['yearbox_months_jun']    = '6月';
+$lang['yearbox_months_jul']    = '7月';
+$lang['yearbox_months_aug']    = '8月';
+$lang['yearbox_months_sep']    = '9月';
+$lang['yearbox_months_oct']    = '10月';
+$lang['yearbox_months_nov']    = '11月';
+$lang['yearbox_months_dec']    = '12月';
+$lang['yearbox_days']          = '日月火水木金土';

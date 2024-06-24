834481bcc00743a2a0bfb481da661def3e2c91f9
Angel <kurti.naitis@gmail.com>
2024-06-24 13:45:45 +0200

    translation update

---
+++ lang/es/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Angel <kurti.naitis@gmail.com>
+ */
+$lang['namestructure']         = 'El esquema de nomenclatura utilizado para crear nuevas entradas y buscar las entradas que se van a mostrar.';
+$lang['namestructure_o_default'] = '<root> : año-mes : <nombre>-año-mes-día';
+$lang['namestructure_o_YearMonthSeperatedNS'] = '<root> : año : mes : <nombre>-año-mes-día';
+$lang['namestructure_o_separatedCompletely'] = '<root> : año : mes : día';
+$lang['namestructure_o_YearNS'] = '<root> : año : <nombre>-año-mes-día';

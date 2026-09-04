9782e0dfe49840549171a256b753bcf3f249acd2
Vitol <vitol.s.salmeron@gmail.com>
2026-09-04 07:55:45 +0200

    Translation update (es)

---
+++ lang/es/lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Vitol <vitol.s.salmeron@gmail.com>
+ */
+$lang['yearbox_months_jan']    = 'Ene';
+$lang['yearbox_months_feb']    = 'Feb';
+$lang['yearbox_months_mar']    = 'Mar';
+$lang['yearbox_months_apr']    = 'Abr';
+$lang['yearbox_months_may']    = 'May';
+$lang['yearbox_months_jun']    = 'Jun';
+$lang['yearbox_months_jul']    = 'Jul';
+$lang['yearbox_months_aug']    = 'Ago';
+$lang['yearbox_months_sep']    = 'Sep';
+$lang['yearbox_months_oct']    = 'Oct';
+$lang['yearbox_months_nov']    = 'Nov';
+$lang['yearbox_months_dec']    = 'Dic';
+$lang['yearbox_days']          = 'DLMXJVS';

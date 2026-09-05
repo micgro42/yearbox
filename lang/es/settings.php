1373de1b99b37599786e6ad9ea4d961c76a2b7da
Vitol <vitol.s.salmeron@gmail.com>
2026-09-05 08:00:05 +0200

    Translation update (es)

---
+++ lang/es/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Vitol <vitol.s.salmeron@gmail.com>
+ */
+$lang['namestructure']         = 'El esquema de nombres utilizado para crear nuevas entradas y encontrar las entradas para mostrar';
+$lang['namestructure_o_default'] = '<raíz> : año-mes: <nombre>-año-mes-día';
+$lang['namestructure_o_YearMonthSeperatedNS'] = '<raíz> : año : mes : <nombre>-año-mes-día';
+$lang['namestructure_o_separatedCompletely'] = '<raíz> : año : mes : día';
+$lang['namestructure_o_YearNS'] = '<raíz> : año : <nombre>-año-mes-día';
